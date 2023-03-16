aec6d8ab05388ba1a2f36ced17c4d73d497c07c3
SirBriann <bmaina049@yahoo.com>
2023-03-16 08:41:22 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,5 @@
 # include <stdio.h>
+# include <stdlib.h>
 /**
  * main - Prints a string followed by standard error
  *
@@ -6,6 +7,6 @@
  */
 int main(void)
 {
-	printf("and that piece of art is useful""\ - Dora Korpar, 2015-10-19");
+	printf("and that piece of art is useful"\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
