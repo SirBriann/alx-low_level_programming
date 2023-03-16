fc5402ef23e0676513185d4b9784e26437b62ed4
SirBriann <bmaina049@yahoo.com>
2023-03-16 06:15:40 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - program that prints a puzzle statement
+ *
+ * Return: Always 0 (Sucesss)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
