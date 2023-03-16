e85c6f6d6e0aa11ac9083a37bde8942edfb0d029
SirBriann <bmaina049@yahoo.com>
2023-03-16 08:26:20 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 # include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
 /**
  * main - Program that prints a string to the stderr
  *
- * Return: Always 0 (success)
+ * Return: Always 1 (success)
  */
 int main(void)
 {
-	printf("and that piece of art is useful""\" - Dora Korpar, 2015-10-19\n");
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
