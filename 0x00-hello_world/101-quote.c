6f86d96a3de54ea50ac58c3723f661b672a8234b
SirBriann <bmaina049@yahoo.com>
2023-03-16 08:04:39 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+# include <stdio.h>
+/**
+ * main - Program that prints a string to the stderr
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("and that piece of art is useful""\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
