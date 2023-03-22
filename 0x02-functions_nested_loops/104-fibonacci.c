041fcec2a620c332d568431d2ea7fbb51ef931da
SirBriann <bmaina049@yahoo.com>
2023-03-22 16:26:07 -0700

    104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,8 +9,8 @@ int main(void)
 {
 	int count = 3;
 
-	unsigned long int first = 1, second = 2;
-	unsigned long int next = first + second;
+	long int first = 1, second = 2;
+	long int next = first + second;
 
 	printf("%lu, ", first);
 	printf("%lu, ", second);
