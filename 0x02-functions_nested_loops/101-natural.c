746a2c4e7fb3be2c21f7750271fbad338db0aecd
SirBriann <bmaina049@yahoo.com>
2023-03-22 15:31:42 -0700

    101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -7,17 +7,23 @@
  */
 int main(void)
 {
-	int n = 0;
+	int sum3 = 0;
+	int sum5 = 0;
 	int i;
 
 	for (i = 0; i < 1024; i++)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
+		if (i % 3 == 0)
 		{
-			n = n + i;
+			sum3 += i;
+		}
+		
+		else if (i % 5 == 0)
+		{
+			sum5 += i;
 		}
 	}
-	printf("%d\n", n);
+	printf("%d\n", sum3 + sum5);
 	return (0);
 }
 
