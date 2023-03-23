dbb408eab95e7922316b88b714bb1b2b5a6d99e6
SirBriann <bmaina049@yahoo.com>
2023-03-23 12:46:01 -0700

    0. Multiple mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - test if positive or negative
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive or negative(0);
+
+	return (0);
+}
