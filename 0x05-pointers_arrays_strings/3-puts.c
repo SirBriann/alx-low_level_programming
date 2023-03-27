43f2ef9331d0236825ec6650f62d8730502617a8
SirBriann <bmaina049@yahoo.com>
2023-03-27 07:41:43 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * _puts - unction that prints a string
+ * @str: Pointer argument
+ * Return: Always (success)
+ */
+void _puts(char *str)
+{
+	printf("%s\n", str);
+}
