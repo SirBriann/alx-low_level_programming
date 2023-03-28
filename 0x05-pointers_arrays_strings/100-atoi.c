045ed3421f69eca3f358ab57b7a6e4efb497cf18
SirBriann <bmaina049@yahoo.com>
2023-03-28 14:54:05 -0700

    My commit

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _atoi - unction that convert a string to an integer
+ * @s: Our argument parameter
+ * Return: Always (success)
+ */
+int _atoi(char *s)
+{
+	_putchar(s);
+
+	return (s);
+}
---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,65 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+#include <string.h>
+
+void randomPasswordGene(int N)
+{
+	int i = 0;
+	int randomizer = 0;
+
+	srand((unsigned int)(time (NULL)));
+	
+	char numbers[] = "0123456789";
+
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+
+	char lettersC[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	char specialS[] = "!@#$%^&*?";
+
+	char password[N];
+
+	randomizer = rand() % 100;
+
+	for (i = 0; i < N; i++)
+	{
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 100;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = specialS[rand() % 8];
+			randomizer = rand() % 100;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = lettersC[rand() % 26];
+			randomizer = rand() % 100;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = letters[rand() % 26];
+			randomizer = rand() % 100;
+			printf("%c", password[i]);
+		}
+	}
+}
+
+int main()
+{
+	int N = 10; 
+	
+	// printf("Enter the length of the password you would want:");
+	// scanf("%d", & N);
+
+	randomPasswordGene(N);
+
+	return (0);
+}
