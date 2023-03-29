84f4125e07dd6d46e5f4923c7d783ea5f44f897b
SirBriann <bmaina049@yahoo.com>
2023-03-29 14:48:10 -0700

    My commit

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,21 +3,23 @@
 #include <string.h>
 
 /**
- * _strncat - a function that concatenates two strings
+ * _strncpy - a function that concatenates two strings
  * @dest: My 1st pointer argument
  * @src: My second pointer argument
  * @n: My third argument n
  * Return: Always (success)
  */
-char *_strncat(char *dest, char *src, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
-	size_t len = strlen(dest);
 	size_t i;
 
-	for (i = 0; n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		dest[len + i] = src[i];
-		dest[len + i] = '\0';
+		dest[i] = src[i];
+		for (; i < n; i++)
+		{
+			dest[i] = '\0';
+		}
 	}
 	return (dest);
 }
