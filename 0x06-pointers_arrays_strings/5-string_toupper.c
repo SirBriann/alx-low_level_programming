edd29e7e7107bfd6fece9f195a22bd5e68898b95
SirBriann <bmaina049@yahoo.com>
2023-03-30 06:26:48 -0700

    My commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+/**
+ * string_toupper -  a function that changes all lowercase letters
+ * @str: Our pointer parameter
+ * Return: Always (success)
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (str[i] >= 65 && str[i] <= 90)
+		str[i] = str[i] - 32;
+		i++;
+	}
+	return (0);
+}
