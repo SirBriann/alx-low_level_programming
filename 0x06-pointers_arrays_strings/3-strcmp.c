a595c88b25657241b71ba9a6fca2ec1a5468ca2c
SirBriann <bmaina049@yahoo.com>
2023-03-30 02:44:52 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,18 +8,16 @@
  * @s2: My second pointer argument
  * Return: Always (success)
  */
-int *_strcmp(char *s1, char *s2)
+int _strcmp(char *s1, char *s2)
 {
-	int i = 0;
+	int i;
 
-	while (s1[i] != '\0' || s2[i] != '\0')
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
 			return (s1[i] - s2[i]);
 		}
-
-		i++;
 	}
-	return (0);
+	return 0;
 }
