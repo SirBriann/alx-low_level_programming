b1778f1a4bfa0e478099ee3ae0cb1d087e189e4d
SirBriann <bmaina049@yahoo.com>
2023-04-06 05:59:39 +0300

    changes

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -11,19 +11,24 @@
  */
 int _sqrt_recursion(int n)
 {
-	n = _sqrt_recursion(n, 1, n / 2);
 
 	if (n < 0)
-	{
 		return (-1);
-	}
-	else if (n== 0 || n == 1)
-	{
+	if (n == 0 || n == 1)
 		return (n);
-	}
+	return (checker(n, 1));
+}
+/**
+ * checker - function that returns the natural square root of a number
+ * @n: Our integer n
+ * Return: Always (success)
+ */
+int checker(int n, int c)
+{
+	if (c * c > n)
+		return (-1);
+	if (c * c == n)
+		return (c);
 
-	else
-	{
-		return _sqrt_recursion(n);
-	}
+	return (checker(n, c + 1));
 }
