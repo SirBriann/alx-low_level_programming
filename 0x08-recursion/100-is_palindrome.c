a9cb2daa9911f3ab845ef30224eda72bf5feb4ca
SirBriann <bmaina049@yahoo.com>
2023-04-07 20:27:03 +0300

    100-is_palindrome.c

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -4,32 +4,40 @@
 /**
  * is_palindrome - function that returns 1 if a string is a palindrome
  * @s: Pointer to the string to be checked
+ * @len: Paremeter for length of string
  * Return: 1 if palindrome and 0 if not
  */
+int helper(char *s, int len);
 int is_palindrome(char *s)
 {
-	int i = 0;
-	int len;
-	int flag = 0;
+	int len = strlen(s);
 
-	len = strlen(s);
+	return helper(s, len);
+}
 
-	for (i = 0; i < len; i++)
-	{
-		if (s[i] != s[len - i - 1])
-		{
-			flag = 1;
-			break;
-		}
-	}
-	if (flag == 1)
-	{
-		return (0);
-	}
-	else
+/**
+ * is_palindrome - function that returns 1 if a string is a palindrome
+ * @s: Pointer to the string to be checked
+ * @len: Paremeter for length of string 
+ * Return: 1 if palindrome and 0 if not
+ */
+
+int helper(char *s, int len)
+{
+	if (len <= 1)
 	{
 		return (1);
 	}
+	
+	else if (s[0] == s[len - 1])
+	{
+		return helper (s + 1, len - 2);
+	}
+	else
+	{
+		return (0);
+	}
 }
+
 
 
