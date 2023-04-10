ffdc931f15d89adefa1d9980f513f48be63e861d
SirBriann <bmaina049@yahoo.com>
2023-04-11 01:06:08 +0300

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,12 @@
  * main - A function that prints its name, followed by a new line.
  * @argc: Our argument counter
  * @argv: Our array list
- * Return: Always (success)
+ * Return: Always 0 (success)
  */
-int main(int argc, __attribute__((unused)),  char *argv[])
+int main(int argc, char *argv[])
 {
+	(void) argc;
+
 	printf("%s\n", argv[0]);
 
 	return (0);
