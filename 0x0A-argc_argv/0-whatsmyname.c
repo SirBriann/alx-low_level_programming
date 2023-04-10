486b900627321f49d2f300dd78c514fba07fdf72
SirBriann <bmaina049@yahoo.com>
2023-04-10 23:39:13 +0300

    My changes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,10 +7,8 @@
  * @argv: Our array list
  * Return: Always (success)
  */
-int main(int argc, char *argv[])
+int main(int argc, __attribute__((unused)),  char *argv[])
 {
-	(void) argc;
-
 	printf("%s\n", argv[0]);
 
 	return (0);
