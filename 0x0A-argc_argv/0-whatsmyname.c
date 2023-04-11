c7d0f1f8f1aaa9e4b8fb571874284a59181f07fb
SirBriann <bmaina049@yahoo.com>
2023-04-11 03:32:21 +0300

    My changes

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,10 +6,8 @@
  * @argv: Our array list
  * Return: Always 0 (success)
  */
-int main(int argc, char *argv[])
+int main(int __attribute__((__unused__)) argc, char *argv[])
 {
-	(void) argc;
-
 	printf("%s\n", argv[0]);
 
 	return (0);
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,4 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
+int main(int argc, argv[]);
+int main(int __attribute__((__unused__)) argc, argv[]);
 int _putchar(char c);
 #endif
