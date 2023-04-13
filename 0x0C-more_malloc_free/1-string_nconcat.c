10447be1368eeea7189b9eb8758e8832faf5bbb1
SirBriann <bmaina049@yahoo.com>
2023-04-13 23:54:39 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -22,7 +22,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		n = len2;
 	}
 
-	str = (char *)calloc(n + len1 + 1, sizeof(char));
+	str = (char *)calloc((n + len1 + 1), sizeof(char));
 
 	if (!str)
 	{
