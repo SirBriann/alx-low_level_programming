a4a2a854fe857ea185ac924c2de4710afc1c00fa
SirBriann <bmaina049@yahoo.com>
2023-04-18 01:32:04 +0300

    dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - Attributes of a dog
+ * @name: 1st attribute
+ * @age: Second attribute
+ * @owner: 3rd attribute
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
