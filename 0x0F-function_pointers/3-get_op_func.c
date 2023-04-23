531ee2bc4bda060b687738132ed160a95c6a9f2a
SirBriann <bmaina049@yahoo.com>
2023-04-23 11:15:38 +0300

    Try this changes

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,14 +20,14 @@ int (*get_op_func(char *s))(int, int)
 		{NULL, NULL}
 	};
 
-	int i;
+	int i = 0;
 
-	for (i = 0; ops[i].op != NULL; i++)
+	while (i < 5)
 	{
-		if (strcmp(ops[i].op, s) == 0)
-		{
+		if (s && s[0] == ops[i].op[0] && !s[1])
 			return (ops[i].f);
-		}
+		i++;
 	}
-	return (0);
+	return (NULL);
 }
+
