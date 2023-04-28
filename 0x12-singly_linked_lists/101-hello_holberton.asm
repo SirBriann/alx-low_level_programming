83bd0b5027983461e65ead0818b7eaba10f4887f
SirBriann <bmaina049@yahoo.com>
2023-04-28 03:30:36 +0300

    101-hello_holberton.asm

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,18 @@
+extern printf
+
+section .data
+	hello: db "Hello, Holberton", 0
+	format: db "%s", 10, 0
+
+section .text
+global main
+main:	
+	push rbp
+	mov rbp, rsp
+	mov rdi, format
+	mov rsi, hello
+	xor eax, eax
+	call printf
+	xor eax, eax
+	pop rbp
+	ret
