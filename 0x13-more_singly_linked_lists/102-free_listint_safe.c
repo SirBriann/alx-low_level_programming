bb9a01f4981f1a8c1e452e9e253841ef82342304
SirBriann <bmaina049@yahoo.com>
2023-05-03 02:30:49 +0300

    102-free_listint_safe.c

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -22,15 +22,16 @@ size_t free_listint_safe(listint_t **h)
 	{
 		next = current->next;
 		printf("[%p] %d\n", (void *)current, current->n);
+
+		if (current >= next)
+		{
+			printf("-> [%p] %d\n", (void *)next, next->n);
+			free (current);
+			break;
+		}
 		free(current);
 		current = next;
 		count++;
-
-		if (current <= next)
-		{
-			printf("-> [%p] %d\n", (void *)current, current->n);
-			break;
-		}
 	}
 	*h = NULL;
 
