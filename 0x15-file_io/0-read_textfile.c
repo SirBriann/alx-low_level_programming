6474f49fb3ef38a1bf35eb7a916fdc531838933f
SirBriann <bmaina049@yahoo.com>
2023-05-09 00:46:48 +0300

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * read_textfile - function that reads a text file and prints it to POSIX
+ * @filename: The poointer to the file
+ * @letters: The number of letters it should read and print
+ * Return: Returns the count.
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	FILE *fp = NULL;
+	size_t i = 0;
+	int ch;
+
+
+	if (!filename)
+	{
+		return (0);
+	}
+
+	fp = fopen(filename, "r");
+
+	if (!fp)
+	{
+		return (0);
+	}
+	ch = fgetc(fp);
+
+	while (ch != EOF && i < letters)
+	{
+		fputc(ch, stdout);
+		i++;
+	}
+	fclose(fp);
+
+	return (i);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+c - FILE I/O
