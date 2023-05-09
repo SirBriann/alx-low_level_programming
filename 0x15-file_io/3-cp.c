228bfa42bb08d3b13bfa17d418c3ae42a2aeab45
SirBriann <bmaina049@yahoo.com>
2023-05-09 05:32:56 +0300

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/stat.h>
+
+#define BUF_SIZE 1024
 /**
  * main - program that copies the content of a file to another file.
  * @argc: Counter
@@ -9,49 +14,55 @@
  */
 int main(int argc, char *argv[])
 {
-	FILE *file_from = NULL;
-	FILE *file_to = NULL;
-	char buffer[1024];
-	size_t n;
+	int file_from;
+	int file_to;
+	char buf[BUF_SIZE];
+	int n;
+	mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
+		dprintf(STDERR_FILENO, "Usage: %s file_from file_to\n", argv[0]);
 		exit(97);
 	}
-	file_from = fopen(argv[1], "r");
+	file_from = open(argv[1], O_RDONLY);
 
-	if (!file_from)
+	if (file_from == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	file_to = fopen(argv[2], "w");
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, mode);
 
-	if (!file_to)
+	if (file_to == -1)
 	{
-		fprintf(stderr, "Error: Can't write to file %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", argv[2]);
 		exit(99);
 	}
-	while ((n = fread(buffer, 1, sizeof(buffer), file_from)) > 0)
+	while ((n = read(file_from, buf, BUF_SIZE)) > 0)
 	{
-		if (fwrite(buffer, 1, n, file_to) != n)
+		if (write(file_to, buf, n) != n)
 		{
-			fprintf(stderr, "Error: Can't writer to file %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't writer to file %s\n", argv[2]);
 			exit(99);
 		}
 	}
-	if (fclose(file_from) != 0 || fclose(file_to) != 0)
+	if (n == -1)
 	{
-		fprintf(stderr, "Error: Can't close fd %d\n",
-			(fileno(file_from) != 0) ? fileno(file_from) :
-		       (fileno(file_to)));
+		dprintf(STDERR_FILENO, "Erro: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+
+	if (close(file_from) == -1 || close(file_to) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n",
+			(file_from != -1) ? file_from : file_to);
 		exit(100);
+	}
+	if (chmod(argv[2], mode) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't set file permission for %s\n", argv[2]);
+		exit(101);
 	}
 	return (0);
 }
-
-
-
-
-
